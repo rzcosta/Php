180f6bce13e90a4011e0d8ad979c92fe82e0437c
Ryan Z. Costa <ryanzanec@gmail.com>
2016-06-09 23:51:23 -0400

    Create testing.php

---
+++ testing.php
@@ -0,0 +1,23 @@
+<?php
+  $hamburger = 4.95;
+  $milkshake = 1.95;
+  $cola = 0.85;
+
+  $tax_rate= 0.075;
+  $tip_rate= 0.16;
+
+  $dinner = (2 * $hamburger) + $milkshake + $cola;
+
+  $tip= $dinner * $tip_rate;
+  $tax= $dinner * $tax_rate;
+  $total = $tax + $tip + $dinner;
+
+
+
+
+
+
+  print ''
+  print 'The Grand total is $' . $total;
+
+?>
